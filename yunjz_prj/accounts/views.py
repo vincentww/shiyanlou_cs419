17f24e24aa5d14361f09f2a8a55d09987aa3067a
Vincent <vincent.wang@kutesmart.com>
2018-06-05 15:41:31 +0800

    MVC

--- yunjz_prj/accounts/views.py
+++ yunjz_prj/accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+#coding=utf-8
+#django package
+#copyright by Vincent
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.contrib.auth.models import User  
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as auth_login ,logout as auth_logout
+from django.core.urlresolvers import reverse
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponseRedirect
+
+#myApp package
+from accounts.forms import  RegisterForm
+from jizhang.data_format_func import auto_gen_categories
+
+def index(request):
+	username = "vincent"
+	return render(request, "accounts/index.html", {"username": username)
+
+def register(request):
+	'''注册视图'''
+	if request.method == "post":
+		#注册完毕，直接登录
+		return HttpResponseRedirect("/accounts/index")
+	return render(request, "accounts/register.html")
+
+def login(request):
+	'''登录视图'''
+	template_var = {}
+	if request.method == "post":
+		username = request.POST.get("username")
+		template_var = {"error": "must first regitster", "username": username}
+	return render(request, "accounts/login.html", template_var)
+
+def logout(request):
+	return render(request, "accounts/logout.html")
+
+
--- yunjz_prj/yunjz/urls.py
+++ yunjz_prj/yunjz/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include(accounts.urls, namespace=('accounts'))),
+    url(r'^jizhang/', include(jizhang.urls, namespace=('jizhang'))),
 ]
